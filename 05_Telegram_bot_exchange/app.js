9e541804e5f71ef8565e4efd5babc6031b6d52d4
Volodymyr Lysenko <lusiy321@gmail.com>
2023-10-31 11:55:46 +0200

    final

--- 05_Telegram_bot_exchange/app.js
+++ 05_Telegram_bot_exchange/app.js
@@ -1,51 +1,83 @@
 import TelegramBot from "node-telegram-bot-api";
 import axios from "axios";
+import NodeCache from "node-cache";
 
 const token = "6929875979:AAFby2qtxXuOceiZzH3j8cpuv_vZbG75IkM";
 
 const bot = new TelegramBot(token, { polling: true });
-
+const cache = new NodeCache({ stdTTL: 300 });
 const apiKey = "0d7f0074167484aea1ac4b73fdfcf8db";
 const city = "Kyiv";
-let interval = 3;
+
 console.log("Bot started");
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    "Hello! Clik to the button 'Forecast in Kyiv' and choose a weather forecast interval on keyboard.",
-    {
-      reply_markup: {
-        keyboard: [["Forecast in Kyiv"]],
-        resize_keyboard: true,
-      },
-    }
-  );
-});
-
-bot.onText(/Forecast in Kyiv/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Select the interval:", {
+  bot.sendMessage(chatId, "Доброго дня! Оберіть дію в меню.", {
     reply_markup: {
-      keyboard: [["3 hours", "6 hours"]],
+      keyboard: [["Погода"], ["Курс валют"]],
       resize_keyboard: true,
     },
   });
 });
 
-bot.onText(/3 hours/, (msg) => {
-  sendWeather(msg, 3);
+bot.onText(/Погода/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Обеіть інтервал:", {
+    reply_markup: {
+      keyboard: [
+        ["Кожні 3 години", "Кожні 6 годин"],
+        ["Вітер"],
+        ["Попередне меню"],
+      ],
+      resize_keyboard: true,
+    },
+  });
+});
+
+bot.onText(/Курс валют/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Оберіть валюту:", {
+    reply_markup: {
+      keyboard: [["USD", "EUR"], ["Попередне меню"]],
+      resize_keyboard: true,
+    },
+  });
+});
+
+bot.onText(/Попередне меню/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Оберіть дію в меню.", {
+    reply_markup: {
+      keyboard: [["Погода"], ["Курс валют"]],
+      resize_keyboard: true,
+    },
+  });
+});
+
+bot.onText(/Вітер/, (msg) => {
+  sendWeather(msg);
+});
+
+bot.onText(/Кожні 3 години/, (msg) => {
+  sendWeather(msg);
   weatherUpdate(msg, 3);
 });
 
-bot.onText(/6 hours/, (msg) => {
-  sendWeather(msg, 6);
-  interval = 6;
+bot.onText(/Кожні 6 годин/, (msg) => {
+  sendWeather(msg);
   weatherUpdate(msg, 6);
 });
 
-function sendWeather(msg, int) {
+bot.onText(/USD/, (msg) => {
+  sendCurrency(msg, "USD");
+});
+
+bot.onText(/EUR/, (msg) => {
+  sendCurrency(msg, "EUR");
+});
+
+function sendWeather(msg) {
   const chatId = msg.chat.id;
   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
@@ -57,15 +89,19 @@ function sendWeather(msg, int) {
       const date = new Date(weather.dt * 1000);
       const temp = weather.main.temp;
       const tempCel = (temp - 273.15).toFixed(0);
-      const description = weather.weather[0].description;
+      const wind = weather.wind.speed;
+      if (msg.text === "Вітер") {
+        const message = `Швидкість вітру у ${city}: ${wind}м/с`;
+        bot.sendMessage(chatId, message);
+      } else {
+        const message =
+          `Зараз погода у ${city}:\n\n` +
+          `Температура повітря: ${tempCel}°C\n` +
+          `Дата: ${date.toISOString()}\n\n` +
+          `${msg.text} Вам буде надходити актуальний прогноз погоди`;
 
-      const message =
-        `Current weather in ${city}:\n\n` +
-        `Temperature: ${tempCel}°C\n` +
-        `Date: ${date.toISOString()}\n` +
-        `Description: ${description}\n`;
-
-      bot.sendMessage(chatId, message);
+        bot.sendMessage(chatId, message);
+      }
     });
   } catch (error) {
     bot.sendMessage(chatId, `Error: ${error.message}`);
@@ -77,4 +113,39 @@ function weatherUpdate(msg, interval) {
     sendWeather(msg, interval);
     weatherUpdate(msg, interval);
   }, interval * 60 * 60 * 1000);
+}
+
+function sendCurrency(msg, currency) {
+  const chatId = msg.chat.id;
+  if (cache.has(currency)) {
+    const rate = cache.get(currency);
+    bot.sendMessage(chatId, `Курс ${currency} до UAH: ${rate}`);
+  } else {
+    fetchCurrency(chatId, currency);
+  }
+}
+async function fetchCurrency(chatId, currency) {
+  const apiUrl = "https://api.monobank.ua/bank/currency";
+
+  try {
+    const res = await axios.get(apiUrl);
+    const rates = res.data;
+    const sortCode = rates.find(
+      (rate) =>
+        rate.currencyCodeB === 980 && rate.currencyCodeA === getCode(currency)
+    );
+    if (sortCode) {
+      const rate = sortCode.rateBuy;
+      cache.set(currency, rate);
+      bot.sendMessage(chatId, `Курс ${currency} до UAH: ${rate}`);
+    } else {
+      bot.sendMessage(chatId, "Не вдалося отримати курси валют.");
+    }
+  } catch (error) {
+    bot.sendMessage(chatId, `Помилка: ${error}. Спробуйте пізніше`);
+  }
+}
+
+function getCode(currency) {
+  return currency === "USD" ? 840 : currency === "EUR" ? 978 : null;
 }
